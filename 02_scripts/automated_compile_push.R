230f584b52320b9c651267bbf6664d60227be806
ashten28 <ashten28@gmail.com>
2020-04-11 17:33:37 +0800

    update scheduler

--- 02_scripts/automated_compile_push.R
+++ 02_scripts/automated_compile_push.R
@@ -5,12 +5,18 @@ wd <- paste0(Sys.getenv('R_USER'), "/Ashten/rprojects/covid19_analysis")
 setwd(wd)
 
 # run data compile script
-system(paste0('Rscript "', wd, '/02_scripts/covid19_data_compile.R"'))
+# system(paste0('Rscript "', wd, '/02_scripts/covid19_data_compile.R"'))
 
 # git commit  and push to github
-system("git add .")
-system('git commit -m "automated update"')
+# system("git add .")
+# system('git commit -m "automated update"')
+# 
+# base::Sys.getenv()
 
-base::Sys.getenv()
+# task scheduler. 
+library(taskscheduleR)
+myscript <- system.file("extdata", "helloworld.R", package = "taskscheduleR")
 
-#    test
+## run script once within 62 seconds
+taskscheduler_create(taskname = "update covid19 data", rscript = myscript,
+                     schedule = "ONCE", starttime = format(Sys.time() + 62, "%H:%M"))
